d8dd2bcc528958d1fe16c2ae21027cac21a4494b
Alexander Quevedo <chanderjose@gmail.com>
2021-01-23 23:03:07 -0400

    Use Math.hypot in Darts solution

--- darts/src/main/groovy/Darts.groovy
+++ darts/src/main/groovy/Darts.groovy
@@ -2,17 +2,15 @@ class Darts {
 
     static int score(x, y) {
         // Using Pythagorean theorem
-        def distance = Math.sqrt ( Math.pow (x, 2) + Math.pow(y, 2))
+        def distance = Math.hypot(x, y)
 
-        int scored
+        int scored = 0
         if(distance <= 1) {
             scored = 10
         } else if (distance > 1 && distance <= 5) {
             scored = 5
-        } else if (distance >5 && distance <= 10) {
+        } else if (distance > 5 && distance <= 10) {
             scored = 1
-        } else {
-            scored = 0
         }
 
         scored
